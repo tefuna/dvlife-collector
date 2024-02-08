999aeb53058fe9ca12e0a987efba14d635c6ba40
tefuna <tefuna@gmail.com>
2024-02-08 13:40:24 +0000

    ネオモバクローズ対応 #11

--- dvlife_collector/domain/model/position.py
+++ dvlife_collector/domain/model/position.py
@@ -22,7 +22,11 @@ class Position:
 
     def __post_init__(self):
         object.__setattr__(self, "position_id", f"{self.ticker}_{self.base_date:%Y%m%d}")
-        object.__setattr__(self, "book_value", self.acquisition_price * self.quantity)
+        # 新株予約等、取得価額が 0 のケースの対応。
+        acquisition_price = self.acquisition_price
+        if self.acquisition_price == 0:
+            acquisition_price = self.current_price
+        object.__setattr__(self, "book_value", acquisition_price * self.quantity)
         object.__setattr__(self, "market_value", self.current_price * self.quantity)
         object.__setattr__(self, "valuation_value", self.market_value - self.book_value)
         object.__setattr__(self, "valuation_ratio", self.valuation_value / self.book_value)
