f6ed30bcb91fa1eaeef55137502c48773bf2c739
tefuna <tefuna@gmail.com>
2025-03-01 14:51:42 +0900

    ruff upgrade and edit scraping tag

--- dvlife_collector/infrastructure/position/page/sbi_page.py
+++ dvlife_collector/infrastructure/position/page/sbi_page.py
@@ -42,7 +42,7 @@ class SbiPage(PageBase):
 
         trs = {}
         result_set = soup.select(
-            "body > div:nth-child(1) > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(2) > td > table:nth-child(1) > tbody > tr > td > form > table:nth-child(3) > tbody > tr:nth-child(1) > td:nth-child(2) > table:nth-child(19) > tbody > tr > td:nth-child(3) > table:nth-child(4) > tbody > tr"  # noqa: E501
+            "body > div:nth-child(3) > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(2) > td > table:nth-child(1) > tbody > tr > td > form > table:nth-child(3) > tbody > tr:nth-child(1) > td:nth-child(2) > table:nth-child(19) > tbody > tr > td:nth-child(3) > table:nth-child(4) > tbody > tr"  # noqa: E501
         )
         trs[ACC_TYPE_TK] = result_set[2:]
         return trs
